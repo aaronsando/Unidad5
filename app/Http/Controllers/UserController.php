ed26e20c17f949f0e6db669ced101f9b1640f919
aaronsando <111078817+aaronsando@users.noreply.github.com>
2022-10-20 16:01:00 -0500

    seeder para insertar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Users;
 
 class UserController extends Controller
 {
@@ -14,7 +15,7 @@ class UserController extends Controller
     public function index()
     {
         //
-        echo "olaaa";
+        return User:: all();
     }
 
     /**
